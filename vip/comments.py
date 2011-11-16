342be2a1548175f3f3a51096e55e3c3f9e700ad0
Nikolay Bachiyski <nb@nikolay.bg>
2011-11-16 23:57:30 +0200

    Get comments from DB

--- vip/comments.py
+++ vip/comments.py
@@ -2,10 +2,11 @@ from trac.core import *
 from trac.web.chrome import INavigationContributor, ITemplateProvider, add_script, add_stylesheet
 from trac.web.main import IRequestHandler, IRequestFilter
 from trac.util import escape, Markup
+from trac.versioncontrol.api import RepositoryManager
 
 class VIPComments(Component):
     implements(INavigationContributor, IRequestHandler, IRequestFilter, ITemplateProvider)
-    
+
     href = 'code-comments'
 
     # INavigationContributor methods
@@ -20,18 +21,29 @@ class VIPComments(Component):
     def match_request(self, req):
         return req.path_info.startswith('/' + self.href)
 
+    def process_request(self, req):
+        data = {}
+
+        rm = RepositoryManager(self.env)
+        data['reponame'], repos, path = rm.get_repository_by_path('/')
+
+        @self.env.with_transaction()
+        def get_comments(db):
+            cursor = db.cursor()
+            cursor.execute("SELECT * FROM vip_comments")
+            data['comments'] = cursor.fetchall()
+        return 'comments.html', data, None
+
+    # ITemplateProvider methods
     def get_templates_dirs(self):
         from pkg_resources import resource_filename
         return [resource_filename(__name__, 'templates')]
-        
+
     def get_htdocs_dirs(self):
         from pkg_resources import resource_filename
         return [('vip', resource_filename(__name__, 'htdocs'))]
 
-
-    def process_request(self, req):
-        return 'comments.html', {}, None
-
+    # IRequestFilter methods
     def pre_process_request(self, req, handler):
         add_script(req, 'vip/x.js')
         return handler
