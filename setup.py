13baa89b5fbaac322d9ba1bcbb7cdede30512251
Nikolay Bachiyski <nb@nikolay.bg>
2011-11-23 14:53:11 +0200

    Rename all vip occurrences to code(_-)comments

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='TracVIP', version='0.1',
+    name='TracCodeComments', version='0.1',
     packages=find_packages(exclude=['*.tests*']),
     entry_points = {
         'trac.plugins': [
-            'vip = vip',
+            'code_comments = code_comments',
         ],
     },
 )
