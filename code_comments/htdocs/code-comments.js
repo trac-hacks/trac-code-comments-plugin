aa5a8d5cb37d8f703b9ed4e91a3bd1c081965fb2
Nikolay Bachiyski <nb@nikolay.bg>
2011-11-23 15:56:45 +0200

    Clean up code-comments.js
    * Proper naming
    * Trailing whitespace

--- code_comments/htdocs/code-comments.js
+++ code_comments/htdocs/code-comments.js
@@ -4,26 +4,25 @@ jQuery(function($) {
 
     window.Comment = Backbone.Model.extend({
     });
-    
+
     window.CommentsList = Backbone.Collection.extend({
         model: Comment,
-        localStorage: new Store("comments"),
         comparator: function(comment) {
             return comment.get('time');
         }
     });
     window.TopComments = new CommentsList;
-    
-    window.CommentView = Backbone.View.extend({
+
+    window.TopCommentView = Backbone.View.extend({
         tagName: 'li',
         template:  _.template($('#comment-template').html()),
         events: {
-           'click .delete': 'del', 
+           'click .delete': 'del',
         },
         initialize: function() {
            this.model.bind('change', this.render, this);
            this.model.bind('destroy', this.remove, this);
-        },                
+        },
         render: function() {
            $(this.el).html(this.template(this.model.toJSON()));
            return this;
@@ -35,28 +34,24 @@ jQuery(function($) {
             this.model.destroy();
          }
     });
-    
+
     window.TopCommentsView = Backbone.View.extend({
-        el: $('#top-comments'),
-        
+
+
         events: {
             'click #add-comment': 'createOnAddButton'
         },
-        
+
         initialize: function() {
             this.textarea = this.$('#comment-text');
-            console.log(this.textarea);
             TopComments.bind('add',   this.addOne, this);
-            TopComments.bind('reset', this.addAll, this);                    
+            TopComments.bind('reset', this.addAll, this);
             TopComments.fetch();
         },
-        
+
         addOne: function(comment) {
-            var view = new CommentView({model: comment});
+            var view = new TopCommentView({model: comment});
             window.x =view.render().el;
-            console.log(this.$);
-            console.log($);
-            console.log(this);
             this.$("ul").append(view.render().el);
         },
         addAll: function() {
@@ -69,6 +64,6 @@ jQuery(function($) {
             TopComments.create({text: text, html: text, author: 'nb', permalink: 'http://dir.bg/',formatted_date: 'Nov 23rd, 2011', time: 5});
         }
     });
-    
-    window.TopCommentsBlock = new TopCommentsView;	
+
+    window.TopCommentsBlock = new TopCommentsView;
 });
