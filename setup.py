5588c853080867556ff78f72d4515208eed14be8
Nikolay Bachiyski <nb@nikolay.bg>
2011-11-24 14:29:13 +0200

    Include data files when building the plugin

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='TracCodeComments', version='0.1.1',
+    name='TracCodeComments', version='0.1.2',
     packages=find_packages(exclude=['*.tests*']),
     entry_points = {
         'trac.plugins': [
             'code_comments = code_comments',
         ],
     },
+    package_data = {'code_comments': ['templates/*.html', 'templates/js/*.html', 'htdocs/*.*',]},
 )
