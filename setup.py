c4ffdd82654664387f0ccc5943348fbcb93ddfc3
Nikolay Bachiyski <nb@nikolay.bg>
2011-11-25 19:28:05 +0200

    Include all data files in 0.1.6

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='TracCodeComments', version='0.1.5',
+    name='TracCodeComments', version='0.1.6',
     packages=find_packages(exclude=['*.tests*']),
     entry_points = {
         'trac.plugins': [
             'code_comments = code_comments',
         ],
     },
-    package_data = {'code_comments': ['templates/*.html', 'templates/js/*.html', 'htdocs/*.*',]},
+    package_data = {'code_comments': ['templates/*.html', 'templates/js/*.html', 'htdocs/*.*','htdocs/jquery-ui/*.*', 'htdocs/jquery-ui/images/*.*']},
 )
