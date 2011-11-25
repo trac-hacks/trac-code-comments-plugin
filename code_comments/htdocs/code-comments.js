207a0c1f22d2150b141e87dbc85965d8a4d3d8f3
Nikolay Bachiyski <nb@nikolay.bg>
2011-11-25 17:52:32 +0200

    Fetch the comment after we create so that we get the HTML version

--- code_comments/htdocs/code-comments.js
+++ code_comments/htdocs/code-comments.js
@@ -1,5 +1,21 @@
 jQuery(function($) {
-	window.Comment = Backbone.Model.extend({
+	_.extend(Backbone.Collection.prototype, {
+		createAndFetch : function(model, options) {
+			var coll = this;
+			options || (options = {});
+			model = this._prepareModel(model, options);
+			if (!model) return false;
+			var success = options.success;
+			options.success = function(nextModel, resp, xhr) {
+				nextModel = coll._prepareModel(nextModel, options);
+				nextModel.fetch();
+				console.log(nextModel);
+				coll.add(nextModel, options);
+				if (success) success(nextModel, resp, xhr);
+			};
+			model.save(null, options);
+			return model;
+		}
 	});
 
 	window.CommentsList = Backbone.Collection.extend({
@@ -91,7 +107,7 @@ jQuery(function($) {
 		createComment: function(e) {
 			var text = this.$('textarea').val();
 			if (!text) return;
-			TopComments.create({text: text, author: 'nb', path: CodeComments.path, revision: CodeComments.revision, line: 0});
+			TopComments.createAndFetch({text: text, author: 'nb', path: CodeComments.path, revision: CodeComments.revision, line: 0});
 		},
 	});
 
