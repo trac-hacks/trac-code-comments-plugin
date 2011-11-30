069067c9240f6e1ebb9a0490dd93c17f232a3fb0
Nikolay Bachiyski <nb@nikolay.bg>
2011-11-30 14:54:17 +0200

    Bump 0.1.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='TracCodeComments', version='0.1.7',
+    name='TracCodeComments', version='0.1.8',
     packages=find_packages(exclude=['*.tests*']),
     entry_points = {
         'trac.plugins': [
