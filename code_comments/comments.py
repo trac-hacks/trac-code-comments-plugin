0388643e81a3e48baead41c3d40032bec3ec4bd9
Nikolay Bachiyski <nb@nikolay.bg>
2011-12-02 16:04:31 +0200

    Remove trailing whitespace

--- code_comments/comments.py
+++ code_comments/comments.py
@@ -10,7 +10,7 @@ try:
     import json
 except ImportError:
     import simplejson as json
-    
+
 try:
     import hashlib
     md5_hexdigest = lambda s: hashlib.md5(s).hexdigest()
@@ -23,11 +23,11 @@ VERSION = 1
 
 class Comment:
     columns = [column.name for column in db.schema['code_comments'].columns]
-    
+
     required = 'text', 'author'
-    
+
     _email_map = None
-    
+
     def __init__(self, req, env, data):
         if isinstance(data, dict):
             self.__dict__ = data
@@ -41,10 +41,10 @@ class Comment:
         self.html = format_to_html(self.env, context, self.text)
         email = self.email_map().get(self.author, 'baba@baba.net')
         self.email_md5 = md5_hexdigest(email)
-    
+
     def _empty(self, column_name):
         return not hasattr(self, column_name) or not getattr(self, column_name)
-        
+
     def email_map(self):
         if self._email_map is None:
             self._email_map = {}
@@ -52,7 +52,7 @@ class Comment:
                 if email:
                     self._email_map[username] = email
         return self._email_map
-    
+
     def validate(self):
         missing = [column_name for column_name in self.required if self._empty(column_name)]
         if missing:
@@ -68,7 +68,7 @@ class Comment:
         line_suffix = ''
         if self.line:
             line_suffix = '#L'+str(self.line)
-        return '%s@%s%s' % (self.path, self.revision, line_suffix) 
+        return '%s@%s%s' % (self.path, self.revision, line_suffix)
 
     def trac_link(self):
         return 'source:' + self.path_revision_line()
@@ -127,7 +127,7 @@ class Comments:
                 conditions.append(name + ' > %s')
             elif name.endswith('__lt'):
                 name = name.replace('__lt', '')
-                conditions.append(name + ' < %s')                
+                conditions.append(name + ' < %s')
             else:
                 conditions.append(name + ' = %s')
         conditions_str = ' AND '.join(conditions)
@@ -141,7 +141,7 @@ class Comments:
         comment.validate()
         comment.time = int(time())
         values = [getattr(comment, column_name) for column_name in comment.columns if column_name != 'id']
-        comment_id = [None]        
+        comment_id = [None]
         @self.env.with_transaction()
         def insert_comment(db):
             cursor = db.cursor()
