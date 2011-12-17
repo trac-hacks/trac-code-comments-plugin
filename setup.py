ffe65e8d1d4ebab3b7d325686363aea957ba505f
Nikolay Bachiyski <nb@nikolay.bg>
2011-12-17 19:46:47 +0200

    Add some more metadata

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name='TracCodeComments', version='0.3.6',
+    author='Nikolay Bachiyski',
+    author_email='nikolay@automattic.com',
+    description='Tool for leaving inline code comments',
     packages=find_packages(exclude=['*.tests*']),
     entry_points = {
         'trac.plugins': [
