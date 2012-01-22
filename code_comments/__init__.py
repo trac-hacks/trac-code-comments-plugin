9546af27ecf3c5a426832c16713041fb9fb45509
Nikolay Bachiyski <nb@nikolay.bg>
2012-01-22 21:43:58 +0200

    Specify that the event listener component listens for ticket changes

--- code_comments/__init__.py
+++ code_comments/__init__.py
@@ -2,4 +2,4 @@ from code_comments import comments
 from code_comments import db
 from code_comments import web
 from code_comments import comment_macro
-from code_comments import event_listener
+from code_comments import ticket_event_listener
