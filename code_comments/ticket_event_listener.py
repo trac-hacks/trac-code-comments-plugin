d0ab7dea3532e88f60c05af760aef4901c0be6b8
Nikolay Bachiyski <nb@nikolay.bg>
2012-06-20 00:46:52 +0200

    Remove trailing whitespace

--- code_comments/ticket_event_listener.py
+++ code_comments/ticket_event_listener.py
@@ -14,7 +14,7 @@ class UpdateTicketCodeComments(Component):
 
     def ticket_created(self, ticket):
         self.update_relations(ticket)
-        
+
     def ticket_changed(self, ticket, comment, author, old_values):
         self.update_relations(ticket)
 
@@ -26,7 +26,7 @@ class UpdateTicketCodeComments(Component):
         # (time, author, field, oldvalue, newvalue, permanent)
         changes = ticket.get_changelog()
         description = ticket['description']
-        
+
         comment_ids += re.findall(CodeCommentLinkMacro.re, description)
         if changes:
             for change in changes:
@@ -35,15 +35,15 @@ class UpdateTicketCodeComments(Component):
 
         comment_ids = set(comment_ids)
         comment_ids_csv = ','.join(comment_ids)
-        
-        existing_comments_query = 'SELECT * FROM ticket_custom WHERE ticket = %s AND name = "code_comment_relation"'        
+
+        existing_comments_query = 'SELECT * FROM ticket_custom WHERE ticket = %s AND name = "code_comment_relation"'
         existing_comments = self.fetch(existing_comments_query, [ticket.id])
-        
+
         if existing_comments:
             self.query('UPDATE ticket_custom SET value=%s WHERE ticket=%s AND name="code_comment_relation"', [comment_ids_csv, ticket.id])
         else:
             self.query('INSERT INTO ticket_custom (ticket, name, value) VALUES (%s, "code_comment_relation", %s)', [ticket.id, comment_ids_csv])
-            
+
     def query(self, query, args = [], result_callback=None):
         if result_callback is None:
             result_callback = lambda db, cursor: True
@@ -54,6 +54,6 @@ class UpdateTicketCodeComments(Component):
             cursor.execute(query, args)
             result['result'] = result_callback(db, cursor)
         return result['result']
-        
+
     def fetch(self, query, args = []):
         return self.query(query, args, lambda db, cursor: cursor.fetchall())
