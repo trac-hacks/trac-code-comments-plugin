b5108353f5ab4d8545f232fd2399049d656b1fec
David Murphy <dave@schwuk.com>
2014-08-07 13:33:20 +0100

    Removed superfluous debug logging of notification recipients.

--- code_comments/notification.py
+++ code_comments/notification.py
@@ -133,9 +133,6 @@ class CodeCommentNotifyEmail(NotifyEmail):
         # Remove duplicates
         ccrcpts = ccrcpts.difference(torcpts)
 
-        self.env.log.debug("Will send notification to: %s" % torcpts)
-        self.env.log.debug("Will copy notification to: %s" % ccrcpts)
-
         return (torcpts, ccrcpts)
 
     def _get_author_name(self, comment):
